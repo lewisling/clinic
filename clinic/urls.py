11defdd03d983c9ceed287139413325897106bfc
Valli Chidambaram <vallic@gmail.com>
2014-07-30 13:08:19 -0600

    clinic_notetaker_final_ver
    clinic_notetaker_final_ver

--- clinic/urls.py
+++ clinic/urls.py
@@ -1,32 +1,17 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.conf.urls.static import static
-from notetaker.views import (RegistrationView,RegistrationCompleteView)
 from django.contrib import admin
-from two_factor.urls import urlpatterns as tf_urls
-from two_factor.gateways.twilio.urls import urlpatterns as tf_twilio_urls
-
-from two_factor.admin import AdminSiteOTPRequired
-from two_factor.views import LoginView
 admin.autodiscover()
-
 
 urlpatterns = patterns('',
 	 url(r'^$', 'notetaker.views.home_page', name='home'),
-         url(r'^newnote/$', 'notetaker.views.newnote_page', name='newnote'),
+	 url(r'^register/$', 'notetaker.views.register_user', name='register'),
+	 url(r'^newnote/$', 'notetaker.views.newnote_page', name='newnote'),
 	 url(r'^addpatient/$', 'notetaker.views.addpatient_page', name='addpatient'),
-         url(regex=r'^account/logout/$',view='django.contrib.auth.views.logout',name='logout',),
-         url(regex=r'^account/custom-login/$',view=LoginView.as_view(redirect_field_name='next_page'),name='custom-login',),
-         url(regex=r'^account/register/$',view=RegistrationView.as_view(),name='registration',),
-         url(regex=r'^account/register/done/$',view=RegistrationCompleteView.as_view(),name='registration_complete',),
+         url(r'^admin/', include(admin.site.urls)),
          url(r'^accounts/', include('registration.urls')),
          url(r'^ckeditor/', include('ckeditor.urls')),
-         url(r'', include('two_factor.urls', 'two_factor')),
-         url(r'', include('user_sessions.urls', 'user_sessions')),
-         url(r'', include(tf_urls + tf_twilio_urls, 'two_factor')),
-         url(r'^admin/', include(admin.site.urls)),
-
-        
 )
 
 if settings.DEBUG:
--- notetaker/views.py
+++ notetaker/views.py
@@ -1,24 +1,16 @@
 from django.shortcuts import render, render_to_response, RequestContext
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.http import HttpResponseRedirect    
 from django.contrib import auth                 
 from django.core.context_processors import csrf 
 #from .forms import RegistrationForm
 from .forms import NoteForm
-from two_factor.views import OTPRequiredMixin
-from two_factor.views.utils import class_view_decorator
-from django.conf import settings
-from django.contrib.auth.forms import UserCreationForm
-from django.views.decorators.cache import never_cache
-from django.views.generic import TemplateView, FormView
-
-
 
 # Create your views here.
 def home_page(request):
     return render(request, 'home.html')
-def addpatient_page(request):
-    return render(request, 'addpatient.html')   
+def register_user(request):
+    return render(request, 'register.html')
 def newnote_page(request):
     form = NoteForm(request.POST or None )
     if form.is_valid():
@@ -26,24 +18,8 @@ def newnote_page(request):
         save_it.save()
     return render_to_response('newnote.html', locals(), context_instance = RequestContext(request))
 
-class RegistrationView(FormView):
-    template_name = 'registration.html'
-    form_class = UserCreationForm
-  
-    def form_valid(self, form):
-        form.save()
-        return redirect('registration_complete')
-  
-  
-class RegistrationCompleteView(TemplateView):
-    template_name = 'registration_complete.html'
-  
-    def get_context_data(self, **kwargs):
-        context = super(RegistrationCompleteView, self).get_context_data(**kwargs)
-        context['login_url'] = str(settings.LOGIN_URL)
-        return context
-  
 
 
-
+def addpatient_page(request):
+    return render(request, 'addpatient.html')   
     
